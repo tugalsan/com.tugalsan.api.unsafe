8d94cf89fa1196be3a8092c87920f201657d83a3
tugalsan karabacak <tugalsan@gmail.com>
2022-06-29 10:50:22 +0300

    add fun catchMeIfUCan

--- src/main/java/com/tugalsan/api/unsafe/client/TGS_UnSafe.java
+++ src/main/java/com/tugalsan/api/unsafe/client/TGS_UnSafe.java
@@ -2,13 +2,21 @@ package com.tugalsan.api.unsafe.client;
 
 public class TGS_UnSafe {
 
+    public static void catchMeIfUCan(CharSequence className, CharSequence funcName, Object errorContent) {
+        throw new RuntimeException("CLASS[" + className + "] -> FUNC[" + funcName + "] -> ERR: " + errorContent);
+    }
+
+    public static void catchMeIfUCan(Throwable t) {
+        throw new RuntimeException(t);
+    }
+
     public static void of(TGS_UnSafeExecutable exe) {
         try {
             if (exe != null) {
                 exe.execute();
             }
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            catchMeIfUCan(e);
         }
     }
 }
