b66bd6646329de4927b5f1dbda9c5c4736cd1599
tugalsan karabacak <tugalsan@gmail.com>
2022-09-29 09:20:41 +0300

    jdk19 upgrade

--- src/main/java/com/tugalsan/api/unsafe/server/TS_UnSafe.java
+++ src/main/java/com/tugalsan/api/unsafe/server/TS_UnSafe.java
@@ -1,24 +1,21 @@
 package com.tugalsan.api.unsafe.server;
 
-import com.tugalsan.api.compiler.client.TGS_CompilerType1;
-import com.tugalsan.api.executable.client.TGS_ExecutableType1;
-import static com.tugalsan.api.unsafe.client.TGS_UnSafe.compile;
-import static com.tugalsan.api.unsafe.client.TGS_UnSafe.execute;
-import com.tugalsan.api.unsafe.client.TGS_UnSafeCompiler;
-import com.tugalsan.api.unsafe.client.TGS_UnSafeExecutable;
-import java.util.concurrent.locks.ReentrantLock;
+import com.tugalsan.api.compiler.client.*;
+import com.tugalsan.api.executable.client.*;
+import com.tugalsan.api.unsafe.client.*;
+import java.util.concurrent.locks.*;
 
 public class TS_UnSafe {
 
     public static <R> R compileSafe(TGS_UnSafeCompiler<R> cmp, TGS_CompilerType1<R, Exception> exception) {
         var lock = new ReentrantLock();
         lock.lock();
-        return compile(cmp, exception, () -> lock.unlock());
+        return TGS_UnSafe.compile(cmp, exception, () -> lock.unlock());
     }
 
     public static void executeSafe(TGS_UnSafeExecutable exe, TGS_ExecutableType1<Exception> exception) {
         var lock = new ReentrantLock();
         lock.lock();
-        execute(exe, exception, () -> lock.unlock());
+        TGS_UnSafe.execute(exe, exception, () -> lock.unlock());
     }
 }
