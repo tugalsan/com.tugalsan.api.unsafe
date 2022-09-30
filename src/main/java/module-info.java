07d70a5bd4256243a25f1dacc8774a87f6d7e45d
tugalsan karabacak <tugalsan@gmail.com>
2022-09-30 07:55:08 +0300

    module-info updated

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,4 @@ module com.tugalsan.api.unsafe {
     requires com.tugalsan.api.executable;
     requires com.tugalsan.api.compiler;
     exports com.tugalsan.api.unsafe.client;
-    exports com.tugalsan.api.unsafe.server;
 }
