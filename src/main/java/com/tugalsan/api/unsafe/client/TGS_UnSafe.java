b961707c122158bb601ba6c99bb7b45908286dcb
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-03 15:20:13 +0300

    .

--- src/main/java/com/tugalsan/api/unsafe/client/TGS_UnSafe.java
+++ src/main/java/com/tugalsan/api/unsafe/client/TGS_UnSafe.java
@@ -1,5 +1,10 @@
 package com.tugalsan.api.unsafe.client;
 
+import com.tugalsan.api.callable.client.TGS_CallableType1;
+import com.tugalsan.api.runnable.client.TGS_Runnable;
+import com.tugalsan.api.runnable.client.TGS_RunnableType1;
+import java.util.concurrent.Callable;
+
 public class TGS_UnSafe {
 
     public static RuntimeException toRuntimeException(CharSequence className, CharSequence funcName, Object errorContent) {
@@ -31,5 +36,62 @@ public class TGS_UnSafe {
 
     public static <R> R callValue(R result) {
         return result;
+    }
+
+    //-------------------- INTERRUPTED EXCEPTION ----------------
+    public static void throwIfInterruptedException(Exception e) throws InterruptedException {
+        if (e instanceof InterruptedException) {// U NEED THIS SO STRUCTURED SCOPE CAN ABLE TO SHUT DOWN
+            throw (InterruptedException) e;
+        }
+    }
+
+    public static void run(TGS_UnSafeRunnable exe) throws InterruptedException {
+        run(exe, null);
+    }
+
+    public static void run(TGS_UnSafeRunnable exe, TGS_RunnableType1<Exception> exception) throws InterruptedException {
+        run(exe, exception, null);
+    }
+
+    public static void run(TGS_UnSafeRunnable exe, TGS_RunnableType1<Exception> exception, TGS_Runnable finalExe) throws InterruptedException {
+        try {
+            if (exe != null) {
+                exe.run();
+            }
+        } catch (Exception e) {
+            throwIfInterruptedException(e);
+            if (exception == null) {
+                throw new RuntimeException(e);
+            }
+            exception.run(e);
+        } finally {
+            if (finalExe != null) {
+                finalExe.run();
+            }
+        }
+    }
+
+    public static <R> R call(Callable<R> cmp) throws InterruptedException {
+        return call(cmp, null);
+    }
+
+    public static <R> R call(Callable<R> cmp, TGS_CallableType1<R, Exception> exception) throws InterruptedException {
+        return call(cmp, exception, null);
+    }
+
+    public static <R> R call(Callable<R> cmp, TGS_CallableType1<R, Exception> exception, TGS_Runnable finalExe) throws InterruptedException {
+        try {
+            return cmp.call();
+        } catch (Exception e) {
+            throwIfInterruptedException(e);
+            if (exception == null) {
+                throw new RuntimeException(e);
+            }
+            return exception.call(e);
+        } finally {
+            if (finalExe != null) {
+                finalExe.run();
+            }
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,4 @@ module com.tugalsan.api.unsafe {
     requires com.tugalsan.api.runnable;
     requires com.tugalsan.api.callable;
     exports com.tugalsan.api.unsafe.client;
-    exports com.tugalsan.api.unsafe.server;
 }
