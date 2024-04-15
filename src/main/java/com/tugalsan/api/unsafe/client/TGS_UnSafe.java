55239ce49bec5ee29295c009a79a90667a720beb
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-15 15:51:30 +0300

    Revert "Update TGS_UnSafe.java"
    This reverts commit b52a4a5ce9b8ad405c14c42c14fb6ab493c32b09.

--- src/main/java/com/tugalsan/api/unsafe/client/TGS_UnSafe.java
+++ src/main/java/com/tugalsan/api/unsafe/client/TGS_UnSafe.java
@@ -7,6 +7,18 @@ import java.util.concurrent.Callable;
 
 public class TGS_UnSafe {
 
+    public static RuntimeException toRuntimeException(CharSequence className, CharSequence funcName, Object errorContent) {
+        return new RuntimeException(TGS_UnSafe.class + ".toRuntimeException->CLASS[" + className + "] -> FUNC[" + funcName + "] -> ERR: " + errorContent);
+    }
+
+    public static void thrw(CharSequence className, CharSequence funcName, Object errorContent) {
+        throw toRuntimeException(className, funcName, errorContent);
+    }
+
+    public static <R> R thrwReturns(CharSequence className, CharSequence funcName, Object errorContent) {
+        throw toRuntimeException(className, funcName, errorContent);
+    }
+
     public static void thrw(Throwable t) {
         throw new RuntimeException(t);
     }
@@ -15,10 +27,20 @@ public class TGS_UnSafe {
         throw new RuntimeException(t);
     }
 
+    public static void runNothing() {
+    }
+
+    public static <R> R callNull() {
+        return null;
+    }
+
+    public static <R> R callValue(R result) {
+        return result;
+    }
+
     //-------------------- INTERRUPTED EXCEPTION ----------------
     public static void throwIfInterruptedException(Exception e) {
         if (e instanceof InterruptedException) {// U NEED THIS SO STRUCTURED SCOPE CAN ABLE TO SHUT DOWN
-            //WHERE IS MY thread.current.interrupt !!!! (GWT wont like you
             throw new RuntimeException(e);
         }
     }
