20c45b1e8f110cf433cbaee7a97a74045b047108
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-21 17:17:35 +0300

    Update TGS_UnSafe.java

--- src/main/java/com/tugalsan/api/unsafe/client/TGS_UnSafe.java
+++ src/main/java/com/tugalsan/api/unsafe/client/TGS_UnSafe.java
@@ -4,7 +4,6 @@ import com.tugalsan.api.callable.client.TGS_CallableType1;
 import com.tugalsan.api.runnable.client.TGS_Runnable;
 import com.tugalsan.api.runnable.client.TGS_RunnableType1;
 import java.util.concurrent.Callable;
-import java.util.concurrent.TimeoutException;
 
 public class TGS_UnSafe {
 
@@ -35,16 +34,6 @@ public class TGS_UnSafe {
         }
         if (t.getCause() != null) {
             return isInterruptedException(t.getCause());
-        }
-        return false;
-    }
-
-    public static boolean isTimeoutException(Throwable t) {
-        if (t instanceof TimeoutException) {
-            return true;
-        }
-        if (t.getCause() != null) {
-            return isTimeoutException(t.getCause());
         }
         return false;
     }
